092747d057c8af86a7cdfb1318d05e72cbe6b583
Nikhil Ghadge <nghadge@okta.com>
2020-09-02 15:50:19 -0700

    Initial commit

---
+++ src/main/java/com/assignment1/demo/webcontent/WelcomeController.java
@@ -0,0 +1,20 @@
+package com.assignment1.demo.webcontent;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class WelcomeController {
+
+    @Value("${userBucket.path}")
+    private String userBucketPath;
+
+    @RequestMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("name", userBucketPath);
+        return "hello";
+    }
+}
